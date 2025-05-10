5b9a7363d455be111f8dd68731f54fb6f78fb18b
JJiwonYoon <wldnjs0488@gmail.com>
2025-05-10 20:51:26 +0900

    my xacro

--- neuronbot2_gazebo/launch/spawn_nb2.launch.py
+++ neuronbot2_gazebo/launch/spawn_nb2.launch.py
@@ -1,25 +1,18 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import xacro
 
 def generate_launch_description():
-    # urdf 파일의 절대경로를 직접 지정
-    urdf_path = '/home/oskar/nav2_ws/src/neuronbot2/neuronbot2_description/urdf/test.urdf'
+    # xacro 파일 경로 지정
+    xacro_path = '/home/oskar/nav2_ws/src/neuronbot2/neuronbot2_description/urdf/husky_bot.urdf.xacro'
+
+    # xacro → urdf 변환
+    robot_description_config = xacro.process_file(xacro_path)
+    robot_desc = robot_description_config.toxml()
 
     # Launch arguments
     x_pose = LaunchConfiguration('x_pose', default='0.0')
@@ -32,13 +25,21 @@ def generate_launch_description():
         'y_pose', default_value='0.0',
         description='y position of the robot')
 
-    # exploration_bot 스폰 노드
+    # robot_state_publisher 노드 (필수!)
+    robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}]
+    )
+
+    # Gazebo에 로봇 스폰
     spawn_exploration_bot_cmd = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
             '-entity', 'exploration_bot',
-            '-file', urdf_path,
+            '-topic', 'robot_description',
             '-timeout', '300',
             '-x', x_pose,
             '-y', y_pose,
@@ -47,10 +48,10 @@ def generate_launch_description():
         output='screen',
     )
 
-
     ld = LaunchDescription()
     ld.add_action(declare_x_position_cmd)
     ld.add_action(declare_y_position_cmd)
+    ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_exploration_bot_cmd)
 
     return ld
